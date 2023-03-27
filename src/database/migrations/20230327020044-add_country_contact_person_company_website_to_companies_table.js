a3aeeba17b2d4296aca22a4e517633bf307fee36
Ctrixx <ctrixx01@gmail.com>
2023-03-27 03:04:21 +0100

    Added migration files

---
+++ src/database/migrations/20230327020044-add_country_contact_person_company_website_to_companies_table.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn("companies", "country", Sequelize.STRING);
+    await queryInterface.addColumn("companies", "contact_person", Sequelize.STRING);
+    await queryInterface.addColumn("companies", "company_website", Sequelize.STRING);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
